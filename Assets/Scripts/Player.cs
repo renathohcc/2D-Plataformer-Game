f29269a27bd8b827f1ef635ea4537d3c3264f3fa
Renatho Henrique Campos das Chagas <renathohcc@hotmail.com>
2021-03-16 22:35:05 -0300

    Animation and Camera
    Added animation and camera system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,10 +25,14 @@ public class Player : MonoBehaviour
     public float yWallForce;
     public float wallJumpTime;
 
+    Animator anim;
+
     void Start()
     {
         //To start the Rigidbody2D component in the player gameObject;
         rb = GetComponent<Rigidbody2D>();
+        //To start the animator component in the player gameObject;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -45,16 +49,32 @@ public class Player : MonoBehaviour
             Flip();
         }
 
+        //Code to control the "isRunning" variable to make the idle and run animations
+        if(input != 0){
+            anim.SetBool("isRunning", true);
+        }else{
+            anim.SetBool("isRunning", false);
+        }
+
         //Check if player is in the Ground
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        //Code to control the "isJumping" variable to make the player jump animation
+        if(isGrounded == true){
+            anim.SetBool("isJumping", false);
+        }else{
+            anim.SetBool("isJumping", true);
+        }
 
         //Make the player jump
         if(Input.GetKeyDown(KeyCode.UpArrow) && isGrounded == true){
             rb.velocity = Vector2.up * jumpForce;
         }
 
+        //Check if player is in the wall
         isTouchingFront = Physics2D.OverlapCircle(frontCheck.position, checkRadius, whatIsGround);
 
+        //Make the player slide in the wall
         if(isTouchingFront == true && isGrounded == false && input != 0){
             wallSliding = true;
         }else{
@@ -65,6 +85,7 @@ public class Player : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
         }
 
+        //Make the player jump in the wall
         if(Input.GetKeyDown(KeyCode.UpArrow) && wallSliding == true){
             wallJumping = true;
             Invoke("SetWallJumpingToFalse", wallJumpTime);
@@ -82,6 +103,7 @@ public class Player : MonoBehaviour
         facingRight = !facingRight;
     }
 
+    //Function to make the player back to slide after JumpWall
     void SetWallJumpingToFalse(){
         wallJumping = false;
     }
