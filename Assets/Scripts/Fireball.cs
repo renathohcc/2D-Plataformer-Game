49299d89169b1be9e9a302d52e3a774145f32c8b
Renatho Henrique Campos das Chagas <renathohcc@hotmail.com>
2021-03-20 14:58:57 -0300

    Wizard Enemy (Shooter)
    Added an enemy that can shoot some projectils

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public int damage;
+    public float lifeTime;
+    void Start()
+    {
+        //Destroy the projectil if it don't hit any object after some time
+        Destroy(gameObject, lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Make the projectil move
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    //Function to apply damage and destroy the projectil when it touch the Player
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player"){
+            collision.GetComponent<Player>().TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Wizard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wizard : Enemy
+{
+    // Start is called before the first frame update
+    public GameObject fireball;
+    public float timeBetweenShots;
+    float nextShotTime;
+    public Transform shotPoint;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Make the Wizard enemy spawn projectils with a time between the shots
+        if(Time.time > nextShotTime){
+            Instantiate(fireball, shotPoint.position, transform.rotation);
+            nextShotTime = Time.time + timeBetweenShots;
+        }
+    }
+}
