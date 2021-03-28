d1fda09a9045c3df38bbd6552c1b82240593458a
Renatho Henrique Campos das Chagas <renathohcc@hotmail.com>
2021-03-28 17:16:06 -0300

    [ADD] MainMenu
    Added a Menu with buttons and a FadeIn effect in the scene loading

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject panel;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Function to load new scenes
+    public void LoadScene(string sceneName){
+        StartCoroutine(FadeIn(sceneName));
+    }
+
+    public void ExitGame(){
+        Application.Quit();
+    }
+
+    //To make the FadeIn effect
+    IEnumerator FadeIn(string sceneName){
+        panel.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
